600de9822de58529fd3705f7f1cc0e2076504632
ondrejhlavacka <ondrej.hlavacka@blindspot.ai>
2024-12-02 12:00:47 +0100

    2024 Day 2 solved

--- src/Year2024/Day02.hs
+++ src/Year2024/Day02.hs
@@ -1,34 +1,53 @@
 module Year2024.Day02 (solve) where
 
+import Data.List (inits, sort, tails)
+
 {- | Part 1
 
-Example:
+Example should be 2:
 
->>> let example = ""
+>>> let example = "7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9"
 >>> part1 example
+2
 -}
 part1 :: String -> Int
-part1 = undefined
+part1 = length . filter isSafeLevel . map (map read . words) . lines
+
+isSafeLevel :: [Int] -> Bool
+isSafeLevel xs = isSorted xs && isSafeLeap xs
+
+isSorted :: [Int] -> Bool
+isSorted xs = xs == sort xs || reverse xs == sort xs
+
+isSafeLeap :: [Int] -> Bool
+isSafeLeap xs = all inRange $ zipWith (\a b -> abs (a - b)) xs $ tail xs
+  where
+    inRange x = 1 <= x && x <= 3
 
 {- | Part 2
 
-Example:
+Example should be 4:
 
->>> let example = ""
+>>> let example = "7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9"
 >>> part2 example
+4
 -}
 part2 :: String -> Int
-part2 = undefined
+part2 s = part1 s + (length . filter isSafeWithouOne . unsafe $ s)
+  where
+    unsafe :: String -> [[Int]]
+    unsafe = filter (not . isSafeLevel) . map (map read . words) . lines
 
-pInput :: String -> a
-pInput = undefined
+isSafeWithouOne :: [Int] -> Bool
+isSafeWithouOne = any isSafeLevel . genListsWithouOne
+
+-- >>> genListsWithouOne [1, 2, 3, 4, 5]
+genListsWithouOne :: [a] -> [[a]]
+genListsWithouOne xs =
+    zipWith (++) (map init . tail . inits $ xs) (map tail . init . tails $ xs)
 
 solve :: IO ()
 solve = do
-    -- example <- readFile "./src/Year2024/data/day02-example.txt"
-    -- print $ "Part1 example: " ++ show (part1 example)
-    -- print $ "Part2 example: " ++ show (part2 example)
-
     content <- readFile "./src/Year2024/data/day02.txt"
     print $ "Part1 solution: " ++ show (part1 content)
     print $ "Part2 solution: " ++ show (part2 content)
