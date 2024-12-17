cb7afba0e2a0f3c775e6e8eab910b144041c5a25
ondrejhlavacka <ondrej.hlavacka@blindspot.ai>
2024-12-17 18:04:02 +0100

    2024 day 17 part 1 solved

--- src/Year2024/Day17.hs
+++ src/Year2024/Day17.hs
@@ -1,27 +1,190 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+
 module Year2024.Day17 (solve) where
+
+import Data.Bits (Bits (xor))
+import Data.Char (digitToInt)
+import Data.List.Split (chunk)
+import Utils (itemAt)
 
 {- | Part 1
 
 Example:
 
->>> let example = ""
->>> part1 example
+>>> let exampleF = "Register A: 729\nRegister B: 0\nRegister C: 0\n\nProgram: 0,1,5,4,3,0"
+>>> let example1 = "Register A: 0\nRegister B: 0\nRegister C: 9\n\nProgram: 2,6"
+>>> let example2 = "Register A: 10\nRegister B: 0\nRegister C: 0\n\nProgram: 5,0,5,1,5,4"
+>>> part1 example1
+>>> part1 example2
+>>> part1 exampleF
+Just (Program (A: 0,B: 1,C: 9) (InstructionPointer 1) [InstructionCombo Code2 6] (Output []))
+Just (Program (A: 10,B: 0,C: 0) (InstructionPointer 3) [InstructionCombo Code5 0,InstructionCombo Code5 1,InstructionCombo Code5 4] (Output [0,1,2]))
+Just (Program (A: 0,B: 0,C: 0) (InstructionPointer 3) [InstructionCombo Code0 1,InstructionCombo Code5 4,InstructionCombo Code3 0] (Output [4,6,3,5,6,3,5,2,1,0]))
 -}
-part1 :: String -> Int
-part1 = undefined
+part1 :: String -> Maybe Program
+part1 s = do
+    prg <- pInput s
+    return $ runProgram prg
+
+data Program
+    = Program
+        (Register 'A, Register 'B, Register 'C)
+        InstructionPointer
+        [InstructionCombo]
+        Output
+    deriving (Show)
+
+newtype Output = Output [Int] deriving (Show, Eq)
+
+newtype InstructionPointer = InstructionPointer Int deriving (Show, Eq, Ord)
+
+data InstructionCombo = InstructionCombo OpCode Int deriving (Show, Eq)
+
+data OpCode = Code0 | Code1 | Code2 | Code3 | Code4 | Code5 | Code6 | Code7
+    deriving (Show, Eq, Ord)
+
+data Register (t :: RegisterType) where
+    RegA :: {getA :: Int} -> Register 'A
+    RegB :: {getB :: Int} -> Register 'B
+    RegC :: {getC :: Int} -> Register 'C
+
+instance Show (Register t) where
+    show (RegA x) = "A: " ++ show x
+    show (RegB x) = "B: " ++ show x
+    show (RegC x) = "C: " ++ show x
+
+data RegisterType = A | B | C deriving (Show, Eq, Ord)
+
+runProgram :: Program -> Program
+runProgram prg = maybe prg runProgram (programStep prg)
+
+programStep :: Program -> Maybe Program
+programStep prg@(Program _ (InstructionPointer p) ics _) = do
+    ic <- itemAt p ics
+    return $ runInstruction ic prg
+
+runInstruction :: InstructionCombo -> Program -> Program
+runInstruction
+    (InstructionCombo oc co)
+    (Program (rA, rB, rC) (InstructionPointer p) ics (Output out')) = case oc of
+        Code0 -> Program (adv combo rA, rB, rC) nip ics nout
+        Code1 -> Program (rA, bxl combo rB, rC) nip ics nout
+        Code2 -> Program (rA, bst combo, rC) nip ics nout
+        Code3 -> case jnz combo rA of
+            Just ip -> Program (rA, rB, rC) ip ics nout
+            Nothing -> Program (rA, rB, rC) nip ics nout
+        Code4 -> Program (rA, bxc rB rC, rC) nip ics nout
+        Code5 -> Program (rA, rB, rC) nip ics (Output $ out' ++ [out combo])
+        Code6 -> Program (rA, bdv combo rA, rC) nip ics nout
+        Code7 -> Program (rA, rB, cdv combo rA) nip ics nout
+      where
+        nip = InstructionPointer (p + 1)
+        nout = Output out'
+        combo = case co of
+            0 -> 0
+            1 -> 1
+            2 -> 2
+            3 -> 3
+            4 -> getA rA
+            5 -> getB rB
+            6 -> getC rC
+            _ -> error "Invalid program."
+
+adv :: Int -> Register 'A -> Register 'A
+adv co (RegA v) = RegA (v `div` (2 ^ co))
+
+bxl :: Int -> Register 'B -> Register 'B
+bxl co (RegB v) = RegB (xor v co)
+
+bst :: Int -> Register 'B
+bst co = RegB (co `mod` 8)
+
+jnz :: Int -> Register 'A -> Maybe InstructionPointer
+jnz co (RegA v) = if v == 0 then Nothing else Just (InstructionPointer co)
+
+bxc :: Register 'B -> Register 'C -> Register 'B
+bxc (RegB vb) (RegC vc) = RegB (xor vb vc)
+
+out :: Int -> Int
+out co = co `mod` 8
+
+bdv :: Int -> Register 'A -> Register 'B
+bdv co (RegA v) = RegB (v `div` (2 ^ co))
+
+cdv :: Int -> Register 'A -> Register 'C
+cdv co (RegA v) = RegC (v `div` (2 ^ co))
 
 {- | Part 2
 
 Example:
 
->>> let example = ""
+>>> let exampleS = "Register A: 117440\nRegister B: 0\nRegister C: 0\n\nProgram: 0,3,5,4,3,0"
+>>> let example = "Register A: 2024\nRegister B: 0\nRegister C: 0\n\nProgram: 0,3,5,4,3,0"
 >>> part2 example
+Just (Program (A: 253,B: 0,C: 0) (InstructionPointer 1) [InstructionCombo Code0 3,InstructionCombo Code5 4,InstructionCombo Code3 0] (Output []))
 -}
-part2 :: String -> Int
-part2 = undefined
+part2 :: String -> Maybe Program
+part2 s = do
+    prg <- pInput s
+    let solPrg = searchRegA 0 prg
+    return solPrg
 
-pInput :: String -> a
-pInput = undefined
+searchRegA :: Int -> Program -> Program
+searchRegA rA prg = case runProgram' prg of
+    Just sol -> sol
+    Nothing -> searchRegA (rA + 1) (regAProgramCopy (rA + 1) prg)
+  where
+    baseOut = getOuput prg
+
+    runProgram' prg' = case programStep prg' of
+        Just nprg@(Program _ _ _ (Output output)) ->
+            if output == baseOut
+                then Just nprg
+                else
+                    if continue output baseOut
+                        then runProgram' nprg
+                        else Nothing
+        Nothing -> Nothing
+
+continue :: [Int] -> [Int] -> Bool
+continue o1 o2 = o1 == take (length o1) o2
+
+getOuput :: Program -> [Int]
+getOuput (Program _ _ _ (Output output)) = output
+
+regAProgramCopy :: Int -> Program -> Program
+regAProgramCopy ra (Program (_, rB, rC) ip ics out') =
+    Program (RegA ra, rB, rC) ip ics out'
+
+pInput :: String -> Maybe Program
+pInput s = do
+    [ral, rbl, rcl, "", insts] <- Just $ lines s
+    ["Register", "A:", nA] <- Just $ words ral
+    ["Register", "B:", nB] <- Just $ words rbl
+    ["Register", "C:", nC] <- Just $ words rcl
+    ["Program:", nums] <- Just $ words insts
+    let ops = map (\l -> (head l, last l)) . chunk 2 . filter (/= ',') $ nums
+    let regA = RegA $ read nA
+    let regB = RegB $ read nB
+    let regC = RegC $ read nC
+    let ip = InstructionPointer 0
+    let out' = Output []
+    let ics = map parseIC ops
+    return $ Program (regA, regB, regC) ip ics out'
+
+parseIC :: (Char, Char) -> InstructionCombo
+parseIC (oc, co) = case oc of
+    '0' -> InstructionCombo Code0 (digitToInt co)
+    '1' -> InstructionCombo Code1 (digitToInt co)
+    '2' -> InstructionCombo Code2 (digitToInt co)
+    '3' -> InstructionCombo Code3 (digitToInt co)
+    '4' -> InstructionCombo Code4 (digitToInt co)
+    '5' -> InstructionCombo Code5 (digitToInt co)
+    '6' -> InstructionCombo Code6 (digitToInt co)
+    '7' -> InstructionCombo Code7 (digitToInt co)
+    _ -> error "Malformed instructions"
 
 solve :: IO ()
 solve = do
